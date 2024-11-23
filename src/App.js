15da9fb383b8d476ff5d1de6dbaa97f185e97fd8
Hosea <karanja.mungai23@gmail.com>
2024-11-23 11:36:52 +0300

    generated tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img  alt="my image"/>
+      <h2>About Me</h2>
+      <a href="https://github.com">GitHub</a>
+      <a href="https://linkedin.com">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,51 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test ("displays a top level heading with the text `Hi, I'm _______`",() => {
+    render(<App />);
+
+    const topLevelHeading = screen.getByRole("heading", {
+        name:/hi, i'm/i,
+        exact: false,
+        level: 1,
+    })
+
+    expect (topLevelHeading).toBeInTheDocument()
+})
+test ("displays an image with an alt tex describing the content of the image ", () => {
+    render(<App />);
+
+    const image = screen.getByRole("img", {
+
+    })
+
+    expect (image).toBeInTheDocument();
+    expect (image).toHaveAttribute ("alt", "my image");
+})
+test ("displays a second level heading with the text `About Me`", () => {
+    render(<App />);
+
+    const secondLevelHeading = screen.getByRole("heading", {
+        name:/about me/i,
+        exact: false,
+        level: 2,
+    })
+
+    expect (secondLevelHeading).toBeInTheDocument()
+})
+test("displays a link to github page", () => {
+    render (<App />);
+    const githubLink = screen.getByRole("link", {
+        name:/github/i,
+        exact: true,
+    })
+    expect(githubLink).toBeInTheDocument();
+})
+test("displays a link to linkedin page", () => {
+    render (<App />);
+    const linkedinLink = screen.getByRole("link", {
+        name:/linkedin/i,
+        exact: true,
+    })
+    expect(linkedinLink).toBeInTheDocument();
+})
